db111918cec465ea50abd6f8c30157b80874e73b
lostcoast256 <mothstreet.1109@gmail.com>
2025-02-27 15:39:58 +0700

    Add files via upload
    configs and project restructiring

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { greetingMessage } from '../src/index.js'
+import cli from '../index.js';
 
-console.log('Welcome to the Brain Games!')
+console.log('Welcome to the Brain Games!');
 
-greetingMessage()
+cli();
