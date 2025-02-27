7181ef9f6703af359a477872c0b6a78579fb4f35
lostcoast256 <mothstreet.1109@gmail.com>
2025-02-27 15:43:05 +0700

    Restructuring
    delete index.js as it's functionality was replaced by cli.js and moved to root

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const greetingMessage = () => {
-    const name = readlineSync.question('May I have your name? ');
-
-    console.log(`Hi, ${name}`);
-};
-
-export { greetingMessage }
